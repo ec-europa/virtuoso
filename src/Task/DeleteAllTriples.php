9bbe7cb9ddbe936e32cc6221884ae14f09482d61
Ilias Dimopoulos <idimopoulos@hotmail.com>
2019-01-28 20:28:10 +0200

    Delete all graphs from the triplestore.

--- src/Task/DeleteAllTriples.php
+++ src/Task/DeleteAllTriples.php
@@ -11,7 +11,12 @@ class DeleteAllTriples extends VirtuosoTaskBase {
    * Deletes all triples from the backend.
    */
   public function main() {
-    $this->query('DROP ALL;');
+    $graphs = $this->query('SELECT DISTINCT(?g) WHERE { GRAPH ?g { ?s ?p ?o } } ORDER BY ?g');
+    foreach ($graphs as $graph) {
+      if ($graph_uri = $graph->g->getUri()) {
+        $this->query("CLEAR GRAPH <{$graph_uri}>;");
+      }
+    }
   }
 
 }
